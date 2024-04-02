9085d7d321860ed531872f4a97a20b0983521a64
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2024-04-03 01:09:51 +0530

    Fixed Typo Congratulation -> Congratulations

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,9 +1,11 @@
 export default function Dialog({ gameStatus, modalRef }) {
   return (
     <dialog className="result-modal" ref={modalRef}>
-      {gameStatus === 'won'
-        ? 'Congratulation! You Won!'
-        : 'You Lost!! Try again'}
+      <div>
+        {gameStatus === 'won'
+          ? 'Congratulations! You Won!'
+          : 'You Lost!! Try again'}
+      </div>
 
       <button type="button" onClick={() => window.location.reload()}>
         Restart
